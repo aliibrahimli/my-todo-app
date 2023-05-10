be621d79e15889585dfe9747fb40a2321aae004b
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-11 01:03:47 +0400

    Some errors resolved

--- web.py
+++ web.py
@@ -8,13 +8,18 @@ def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
 
 
 todos = functions.get_todos()
 
-st.title("Gündəlik Planlama")
-st.subheader("Planlama proqramından istifadənin qaydaları. Yeni plan əlavə etmək üçün aşağıda gördüyünüz boşluğa yazıb Enter düyməsinə tıklamağınız kifayətdir. İş bitdikdən sonra isə hər bir yazının qarşısında checkbox-a tıklasanız həmin plan silinəcək. Əgər error olarsa 2 dəfə cəld klik etdikdə silinir.")
-st.write("Bu proqram başlanğıc üçün yazılmışdır")
+st.title("Planlama aplikasiyası")
+st.subheader("Bu aplikasiyadan istifadə edərək gündəlik işlərinizi daha planlı idarə edə bilərsiniz.")
+st.write("Tekst boşluğuna yeni planlarınızı yazın və Enter knopkasına klikləyin.\n"
+         "Hərhansısa bir işi bitirdikdə isə planın qarşısında olan checkbox-a klikləyin həmin plan listdən silinəcək.")
+
+st.text_input(label="Plan əlavə edin", placeholder="yeni planınızı əlavə edin...",
+              on_change=add_todo, key='new_todo', value=st.session_state.get('new_todo', ''))
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -23,6 +28,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-st.text_input(label="Plan əlavə et", placeholder="Yeni plan əlavə edin...",
-              on_change=add_todo, key='new_todo')
